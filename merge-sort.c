9310c45d577672e29f901f0b27c220a8858e012d
krishan-gupta <krishangupta204@gmail.com>
2026-02-04 01:09:37 +0530

    Modified the merge sort.

--- merge-sort.c
+++ merge-sort.c
@@ -1,69 +1,54 @@
-//merge sort
 #include <stdio.h>
 
-/* Function to merge two sorted halves */
-void merge(int arr[], int left, int right){
-    int mid = (left+right)/2;
-    int n1 = mid - left+1;
-    int n2 = right - mid;
-    int l[n1];
-    int r[n2];
-
-    for(int i=0;i<n1;i++){
-        l[i]=arr[left+i];
-    }
-
-    for (int j=0;j<n2;j++){
-        r[j]=arr[mid+j+1];
-    }
-
-    int i=0;
-    int j=0;
-    int k=left;
-
-    while(i<n1 && j<n2){
-        if(l[i]<=r[j]){
-            arr[k]=l[i];
-            i++;
-        }else{
-            arr[k]=r[j];
-            j++;
-        }
-        k++;
-    }
-
-    while(i<n1){
-        arr[k]=l[i];
-        i++;
-        k++;
-    }
-    
-    while(j<n2){
-        arr[k]=r[j];
-        j++;
-        k++;
-    }
-
-}
-
-/* Function to implement Merge Sort */
-void mergeSort(int arr[], int left, int right)
+void merge(int arr[], int low, int mid, int high)
 {
-    if (left < right){
-        int mid = (left + right) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, right);
+    int temp[high - low + 1];
+    int left = low;
+    int right = mid + 1;
+    int k = 0;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+            temp[k++] = arr[left++];
+        else
+            temp[k++] = arr[right++];
     }
+
+    while (left <= mid)
+        temp[k++] = arr[left++];
+
+    while (right <= high)
+        temp[k++] = arr[right++];
+
+    for (int i = 0; i < k; i++)
+        arr[low + i] = temp[i];
 }
 
-/* Main function */
+
+void mS(int arr[], int low, int high){
+    if (low >= high){
+        return;
+    }
+
+    int mid = (low + high) / 2;
+    mS(arr, low, mid);
+    mS(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void mergeSort(int arr[], int n){
+    mS(arr, 0, n - 1);
+}
+
+
 int main(){
     int arr[] = {8, 3, 5, 2, 7,2,5,6,8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    mergeSort(arr, 0, n - 1);
+    mergeSort(arr,n);
     printf("Sorted array:\n");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     return 0;
 }
+
