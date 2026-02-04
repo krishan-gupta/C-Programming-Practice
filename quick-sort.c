ac34abcec72de3df1ca8753e1f01531182c2ed3c
krishan-gupta <krishangupta204@gmail.com>
2026-02-04 23:25:03 +0530

    Improved Logic for Quick Sort.

--- quick-sort.c
+++ quick-sort.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-void swap(int *a, int *b){
-    int temp= *a;
-    *a=*b;
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
     *b = temp;
 }
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; 
-    int i = low - 1; 
-    
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        // Corrected boundary: i <= high - 1
+        while (arr[i] <= pivot && i <= high - 1) {
             i++;
-            swap(&arr[i],&arr[j]);
+        }
+
+        while (arr[j] > pivot && j >= low+1) {
+            j--;
+        }
+
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i+1],&arr[high]);
-
-    return (i + 1);
+    
+    swap(&arr[low], &arr[j]);
+    return j;
 }
 
-void quickSort(int arr[], int low, int high) {
+void qs(int arr[], int low, int high) {
     if (low < high) {
-        int mid = partition(arr, low, high);
-
-        quickSort(arr, low, mid - 1);
-        quickSort(arr, mid + 1, high);
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
     }
 }
 
 
 int main() {
     int arr[13]={30,30,30,45,45,45,20,20,20,55,55,86,92};
-    quickSort(arr, 0, 12);
+    qs(arr, 0, 12);
     printf("\nSorted array: ");
     for (int i = 0; i < 12; i++) {
         printf("%d ", arr[i]);
